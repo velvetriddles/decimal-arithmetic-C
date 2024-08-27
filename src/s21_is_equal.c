18a75cf0ba6fc4c645dd63c0f58fec6457bbd867
schiffoo <nastya.qq@bk.ru>
2024-08-28 01:45:32 +0300

    base

---
+++ src/s21_is_equal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "s21_decimal.h"
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+  int result = 0;
+  s21_decimal_long value1_long = {{0, 0, 0, 0, 0, 0, 0}};
+  s21_decimal_long value2_long = {{0, 0, 0, 0, 0, 0, 0}};
+
+  s21_preparing_long_dec(value_1, value_2, &value1_long, &value2_long);
+
+  result = s21_is_equal_long(value1_long, value2_long);
+  return result;
+}
